7f6ed325df0a8ed103501724a0de8bfb9b520e5a
David Enikuomehin <112134400+eniiku@users.noreply.github.com>
2023-01-25 23:27:51 +0100

    feat(App): create mobile view for quotes section

--- src/App.tsx
+++ src/App.tsx
@@ -36,13 +36,16 @@ const App = () => {
 
       {/* Quote Section */}
 
-      <div className='text-center my-32 bg-indigo-500 -mx-16 p-16 radial-gradient text-textWhite'>
-        <q className='font-zilla font-bold text-[52px] mb-10'>
+      <div
+        className='text-center my-16 lg:my-32 bg-indigo-500 -mx-4 lg:-mx-16 p-4 lg:p-16 radial-gradient
+        text-textWhite'
+      >
+        <q className='font-zilla font-bold text-2xl leading-[1.3] lg:leading-none lg:text-[52px] mb-10'>
           It’s our duty to love the unloved, help the helpless and bring hope to
           the hopeless
         </q>
 
-        <p className='font-montserrat font-medium text-2xl text-right'>
+        <p className='font-montserrat font-medium text-sm lg:text-2xl text-right'>
           - Anthony Douglas Williams
         </p>
       </div>
