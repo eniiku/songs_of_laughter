169705f39fb1418ad16bffc4cec3569067ad9cb6
David Enikuomehin <112134400+eniiku@users.noreply.github.com>
2023-01-31 23:24:45 +0100

    fix(Navbar): fix broken links

--- src/container/Navbar.tsx
+++ src/container/Navbar.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 
 const Navbar = () => {
-  const navLinks = ['home', 'about', 'what we do', 'get involved'];
+  const navLinks = [
+    { text: 'home', link: 'home' },
+    { text: 'about', link: 'about' },
+    { text: 'what we do', link: 'what-we-do' },
+    { text: 'get involved', link: 'contact' },
+  ];
 
   const [isOpen, setIsOpen] = React.useState(false);
 
@@ -18,15 +23,15 @@ const Navbar = () => {
         <nav>
           <ul className='font-space text-lg flex gap-8 text-textBlack'>
             {navLinks.map((navLink) => (
-              <li key={navLink}>
+              <li key={navLink.text}>
                 <button>
                   {/* TODO: set links to bold on hover and change font */}
 
                   <a
-                    href={`#${navLink}`}
+                    href={`#${navLink.link}`}
                     className='uppercase opacity-70 hover:opacity-100 hover:font-medium'
                   >
-                    {navLink}
+                    {navLink.text}
                   </a>
                 </button>
               </li>
@@ -38,7 +43,7 @@ const Navbar = () => {
           className='py-3 px-8 bg-[#125A71] text-textWhite font-bold font-space text-lg
         rounded-full'
         >
-          <a href='donate'>Donate Now</a>
+          <a href='#donate'>Donate Now</a>
         </button>
       </div>
 
@@ -55,15 +60,15 @@ const Navbar = () => {
             <ul>
               {navLinks.map((mobileLinks) => (
                 <li
-                  key={mobileLinks}
+                  key={mobileLinks.text}
                   className='w-full border-y border-y-textWhite border-opacity-40 pl-8 py-5'
                 >
                   <button onClick={handleCloseMenu}>
                     <a
-                      href={`#${mobileLinks}`}
+                      href={`#${mobileLinks.link}`}
                       className='text-xl font-medium text-textWhite capitalize'
                     >
-                      {mobileLinks}
+                      {mobileLinks.text}
                     </a>
                   </button>
                 </li>
